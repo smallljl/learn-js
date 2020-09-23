ab8874385785f23a0e65ea86dc44c861aeefba5f
smallljl <168241296@qq.com>
2020-09-23 20:40:24 +0800

    a

--- parser2/src/parser/ast/Expr.js
+++ parser2/src/parser/ast/Expr.js
@@ -1,13 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-09-21 15:49:29
- * @LastEditTime: 2020-09-22 11:21:21
+ * @LastEditTime: 2020-09-23 16:28:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit  
  * @FilePath: \parser2\src\parser\ast\Expr.js
  */
 const ASTNode = require("./ASTNode");
 const ASTNodeTypes = require("./ASTNodeTypes");
+const table = require("../utils/PriorityTable");
 
 class Expr extends ASTNode {
   constructor(parent){
@@ -16,7 +17,9 @@ class Expr extends ASTNode {
 
   static fromToken(parent, type, token){
     const expr = new Expr(parent);
-    expr.label = 
+    expr.label = token.getValue();
+    expr.lexeme = type;
+    return expr;
   }
 
   /**
@@ -37,23 +40,91 @@ class Expr extends ASTNode {
    * @return {type} 
    */
   static parseExpr(parent,it){
-
+    return Expr.E(it,0);
   }
 
   static E(parent, it, k){
-
+    if(k < table.length -1){
+      return Expr.combine(it,
+        ()=>Expr.E(it,k+1),
+        ()=>Expr.E_(it,k)
+      )
+    } else {
+      return Expr.race(
+        it,
+        ()=> 
+          Expr.combine(
+            it,
+            ()=>Expr.F(it),
+            ()=>Expr.E_(it,k)
+          ),
+        ()=>
+          Expr.combine(
+            it,
+            ()=>Expr.U(it),
+            ()=>Expr.E_(it,k)
+          ),
+        ()=>
+          Expr.combine(
+            it,
+            ()=>Expr.U(it),
+            ()=>Expr.E_(it,k)
+          )
+      );
+    }
   }
 
   static E_(parent, it, k){
+    const token = it.peek();
+    const value = token.getValue();
 
+    if(table[k].indexOf(value) !== -1){
+      it.nextMatch(value);
+      const expr = Expr.fromToken(ASTNodeTypes.BINARY_EXPR, token);
+      expr.addChild(
+        Expr.combine(
+          it,
+          ()=> Expr.E(it, k + 1),
+          ()=> Expr.E_(it, k, it)
+        )
+      );
+
+      return expr;
+    }
+
+    return null;
   }
 
   static F(parent, it){
-
+    const factor = Factor.parse(it);
+    if(factor === null){
+      return null;
+    }
+    if(it.hasNext() && it.peek().getValue() === "("){
+      return CallExpr.parse(factor, it);
+    }
+    return factor;
   }
 
   static U(parent, it){
-    
+    const token = it.peek();
+    const value = token.getValue();
+
+    if(value === "("){
+      it.nextMatch("(");
+      const expr = Expr.parse(it);
+      it.nextMatch(")");
+      return expr;
+    } else if(value === "++" || value === "--" || value === "!"){
+      const t = it.peek();
+      it.nextMatch(value);
+
+      const expr = Expr.fromToken(ASTNodeTypes.UNARY_EXPR, t);
+      expr.addChild(Expr.parse(it));
+      return expr;
+    }
+
+    return null;
   }
 
   static combine(it,funcA,funcB){
